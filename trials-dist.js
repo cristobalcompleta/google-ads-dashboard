fdae3a9e94e411ef55c3339e4cfc07ed2824e6fd
Cristobal <crisc@cookunity.com>
2026-04-27 17:43:10 -0300

    Add campaign trial estimation table to Trial Pacing tab (L4 DoW share)

---
+++ trials-dist.js
@@ -0,0 +1,20 @@
+const DIST_DATA = [
+["brand_search_brandcore_b_keyword","brand",505],
+["brand_search_national_core_exact","brand",458],
+["brand_search_midtier_b_keyword","brand",59],
+["brand_search_brandnoncore_b_keyword","brand",45],
+["brand_search_national_noncore_efficient_broad","brand",11],
+["brand_search_national_noncore_efficient_exact","brand",10],
+["brand_search_national_noncore_inefficient_exact","brand",7],
+["nonbrand_search_genericcore_u_keyword","nonbrand",147],
+["nonbrand_search_competitor_u_keyword","nonbrand",84],
+["nonbrand_search_lifestyle_u_keyword","nonbrand",71],
+["nonbrand_search_discovery_u_keyword","nonbrand",52],
+["nonbrand_search_discovery_u_dsa","nonbrand",31],
+["nonbrand_search_store_u_keyword","nonbrand",15],
+["nonbrand_search_cuisine_u_keyword","nonbrand",8],
+["pmax_nonbrand_search_national_pmax_gmcfocus_pixis_tai_testing","nonbrand",1],
+["shopping_national_mealplans","shopping",157],
+["shopping_national_mealplans_b","shopping",45]
+];
+const DIST_DATES = ["2026-04-20","2026-04-13","2026-04-06","2026-03-30"];
